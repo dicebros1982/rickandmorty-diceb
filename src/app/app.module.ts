83e0e0a965425edd1de7461591405f58c1bd1b26
Thys B <dicebros1982@gmail.com>
2019-12-19 19:18:19 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { CoreModule } from "./modules/core/core.module";
 
 import { AppComponent } from "./app.component";
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, CoreModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
